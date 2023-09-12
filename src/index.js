56888f889b6709410cae4eb07dda952f4af013c1
Jesse Reynolds <jessereynolds18@gmail.com>
2023-09-12 00:15:01 -0500

    make several functions to create a to-do list with interactivity

--- src/index.js
+++ src/index.js
@@ -1,3 +1,25 @@
 document.addEventListener("DOMContentLoaded", () => {
-  // your code here
+  let form = document.querySelector("form")
+  form.addEventListener("submit", (e) => {
+    e.preventDefault();
+    const newTaskDescriptionInput = document.getElementById("new-task-description");
+    const newTaskDescription = newTaskDescriptionInput.value;
+    makeTask(newTaskDescription);
+    form.reset();
+  });
 });
+
+function makeTask(insertTask){
+  let makeLi = document.createElement("li");
+  let btn = document.createElement("button");
+  btn.addEventListener("click", closeTask);
+  makeLi.textContent = `${insertTask} `;
+  btn.textContent = "X";
+  makeLi.appendChild(btn);
+  console.log(makeLi);
+  document.getElementById("tasks").append(makeLi);
+}
+
+function closeTask(e){
+  e.target.parentNode.remove();
+}
